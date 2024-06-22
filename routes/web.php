16977f0588396de49b9235731899865ec71c91f4
ShaDow12M <malindasampath45@gmail.com>
2024-06-22 14:47:57 +0530

    Add the Login Authentication for Home page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
     use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\HomeController;
+    use App\Http\Controllers\Auth\LoginController;
 
     Route::get('/contact', function () {
         return view('contact');
@@ -10,15 +12,19 @@
         return view('index');
     });
 
-    Route::get('/admin', function () {
-        return view('admin');
+    Auth::routes();
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+    // Authentication Routes
+    Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [LoginController::class, 'login']);
+    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+    // Admin Routes
+    Route::middleware(['auth'])->group(function () {
+    Route::get('/home', 'AdminController@index')->name('home');
     });
 
-    Route::get('/login', function () {
-        return view('admin-login');
-    });
-
-    Route::get('/', function () {
-        return view('index');
-    })->name('home');
-?>
+    Auth::routes();
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    
